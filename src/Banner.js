9dbbbfad6506163c5e4e07fac9290b525abb5a83
1997prachi <prachi.princy97@gmail.com>
2020-09-19 23:28:14 +0530

    Adding some more functionality

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './banner.css'
+import { Button } from "@material-ui/core";
 
 const Banner = () => {
     return (
         <div className="banner">
-            
+            <div className="banner__search">
+                <Button className="banner__searchbutton" variant='outline'>Serach dates</Button>
+            </div>
+             
+            <div className="banner__info">
+                <h1>Go Near</h1>
+                <h4>Settle in somwhere new.Discover neraby stays to live,work or just relax</h4>
+                <Button variant='outline'>Explore Nearby</Button>
+           </div>
         </div>
     )
     
